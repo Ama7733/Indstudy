1b0fecaed865709dc3376a88321dda1b41031bd8
Alexander Ma <axma@ualr.edu>
2025-02-11 17:14:15 +0000

    class

--- IndStudy Final/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.querySelector('nav ul');
-const nav = document.querySelector('nav');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide the menu
-    nav.classList.toggle('active'); // Adjust nav padding when menu is active
-});
